23e3335770f72ab6bf66412c27b68ac9c29bf58d
jordanos <y.dereje@yahoo.com>
2021-09-01 14:25:21 +0300

    postgres database connection

--- aves/settings.py
+++ aves/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'user',
+    'content',
 ]
 
 MIDDLEWARE = [
@@ -89,15 +91,25 @@ if DEBUG:
 
             'HOST': 'localhost',
 
-            'PORT': '12345',
+            'PORT': '5432',
         }
     }
 
 else:
-    DATABASES = {
+    DATABASES = {     
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+            'NAME': 'aves',
+
+            'USER': 'postgres',
+
+            'PASSWORD': '12345',
+
+            'HOST': 'localhost',
+
+            'PORT': '5432',
         }
     }
 

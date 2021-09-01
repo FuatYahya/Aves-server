14a3592ec12cc47765769522d9ffe5b85c3a6d5a
jordanos <y.dereje@yahoo.com>
2021-09-01 14:47:00 +0300

    views added

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.conf import UserSettingsHolder
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class UserAPIView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializers = UserSerializer(users, many=True)
+        return Response(serializers.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED) 
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 

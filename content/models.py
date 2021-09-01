7da7dce1617a7736f6e5adb05807a4ba138c49fd
jordanos <y.dereje@yahoo.com>
2021-09-01 14:25:45 +0300

    sample code generated

--- content/models.py
+++ content/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=25)
+    type = models.CharField(max_length=25)
+    key = models.CharField(max_length=16)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True, blank=True)
+    price = models.FloatField(default=0)
+    likes_count = models.IntegerField(default=0)
+    size = models.IntegerField(default=0)
+    discription = models.TextField(blank=False)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=25)
+    phone = models.IntegerField(unique=True)
+    password = models.CharField(max_length=25)
+    dob = models.DateField()
+    registered_at = models.DateTimeField(auto_now_add=True, blank=True)
+    balance = models.FloatField(default=0)
+    likes_count = models.IntegerField(default=0)
+    contents_count = models.IntegerField(default=0)
